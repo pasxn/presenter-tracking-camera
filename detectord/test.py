812c423bdc80da0c38ec468ccb079f188c13a4a3
pasxn <pasansanjula.android@gmail.com>
2022-04-01 09:38:43 +0530

    unit test, FPS counter added

--- detectord/test.py
+++ detectord/test.py
@@ -1,11 +1,17 @@
 import cv2
 import numpy as np
+from imutils.video import FPS
+import time
 import MobileNetSSD
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     localDetector = MobileNetSSD.Detector()
+
+    time.sleep(2.0)
+    fps = FPS().start()
     
+    print("[INFO] start")
     while(cap.isOpened()):
         ret, frame = cap.read()
         localBounderies = localDetector.detect(frame)
@@ -17,6 +23,13 @@ if __name__ == '__main__':
 
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
+
+        fps.update()
+    
+    fps.stop()
+
+    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
     cap.release()
     cv2.destroyAllWindows()
